3a50aa95e2b55fc7e2db15905e61f2089eeb4842
hozer006 <58408686+hozer006@users.noreply.github.com>
2023-04-22 13:35:52 +0700

    init file

---
+++ config.js
@@ -0,0 +1,11 @@
+let config = {
+  emailjs: {
+    serviceID: "service_f04axdb",
+    templateID: "template_nvzsgum",
+    userID: "rbDvcxHiIt6zyso3N"
+  }
+};
+
+
+
+export default config;
---
+++ info.js
@@ -0,0 +1,250 @@
+let info = {
+  name: "Natain Bogdan",
+  logo_name: "Mr.Arm",
+  flat_picture: require("./src/assets/11587.jpg"),
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false,
+    },
+  },
+  description:
+    "I am a student who likes in the program as someone who likes to study and develop their own peopleand experience I am always ready to develop myself and experience. and like a challenge",
+  links: {
+    linkedin: "https://www.facebook.com/aunwat.rattanawong",
+    github: "https://github.com/hozer006",
+    angellist: "https://www.instagram.com/16_66411_2344/",
+  },
+  education: [
+    {
+      name: "Sribunruang Vocational College",
+      place: "Computer Business",
+      date: "Graduated - May 2020",
+      degree: "High vocational Certificate",
+      gpa: "GPA 3.81 / 4.0",
+
+      skills: [
+        // "Skills gained:",
+        // "Design, conduct and interpret research and statistical analysis. ",
+        // "Apply an evidence-based approach to problems.",
+        // "Communicate findings using models, charts and graphs.",
+        // "Communicate research findings using clear, accurate, concise writing.",
+      ],
+    },
+    {
+      name: "Udon Thani Rajabhat University",
+      place: "Computer communication Engineering",
+      date: "Graduated - May 2023",
+      degree: "Bachelor's",
+      gpa: "GPA 3.7 / 4.0",
+
+      skills: [
+        // "Skills gained:",
+        // "Design, conduct and interpret research and statistical analysis. ",
+        // "Apply an evidence-based approach to problems.",
+        // "Communicate findings using models, charts and graphs.",
+        // "Communicate research findings using clear, accurate, concise writing.",
+      ],
+    },
+  ],
+  experience: [
+    {
+      name: "Full stack developer",
+      place: "Happnn .inc",
+      date: " 2021 - 2022",
+      position: "Junior",
+      description:
+        "Main task create website structure and front end stying according to design site and back end develop various system according to need such as chat system, live broadcast system, agenda system, etc",
+      // skills: [
+      //   "C++",
+      //   "Python",
+      //   "ArduinoIDE",
+      //   "PlatformIO",
+      //   "HTTP, MQTT, HTTPS, SSH, Modbus",
+      //   "Firebase",
+      //   "Vue.js",
+      // ],
+    },
+
+    {
+      name: "IoT project Engineer",
+      place: "TRINERGY COMM-THA .inc",
+      date: "2021 - 2022",
+      position: "IoT engineer",
+      description:
+        "The front end manages the assigned futures and front end styles based on figma. And Analyze the system according to the flow that has been laid and create API according to the flow.",
+      // skills: [
+      //   "Vue.js",
+      //   "Nuxt.js",
+      //   "React.js",
+      //   "Node.js",
+      //   "MongoDB",
+      //   "MySQL",
+      //   "PostgreSQL",
+      //   "SCSS",
+      //   "HTML",
+      //   "CSS",
+      // ],
+    },
+
+    {
+      name: "Full stack developer",
+      place: "TRINERGY COMM-THA .inc",
+      date: "2022 - 2023",
+      position: "Intern",
+      description:
+        "Design diagrams and procuring equipment. and drafting circuits , etc and software responsible for data storage, Deisplay via web app, coding Calculate BigO and database desing and storage methods.",
+      // skills: [
+      //   "Vue.js",
+      //   "Nuxt.js",
+      //   "React.js",
+      //   "Node.js",
+      //   "MongoDB",
+      //   "MySQL",
+      //   "PostgreSQL",
+      //   "SCSS",
+      //   "HTML",
+      //   "CSS",
+      // ],
+    },
+  ],
+  skills: [
+    {
+      title: "languages",
+      info: ["C++", "JavaScript ES6-8", "Golang/net", "Python"],
+      icon: "fa fa-code",
+    },
+    {
+      title: "IoT Platform",
+      info: ["Blynk", "ThingsBoard",],
+      icon: "fa fa-hashtag",
+    },
+    {
+      title: "web technologies",
+      info: [
+        "Vue.js",
+        "Nuxt.js",
+        "Next.js",
+        "Angualar",
+      ],
+      icon: "fas fa-laptop-code",
+    },
+    {
+      title: "databases",
+      info: ["MongoDB", "MySQL", "PostgreSQL",],
+      icon: "fa fa-database",
+    },
+    {
+      title: "Tools",
+      info: [
+        "AWS EC2, AWS IoT Core","AWS S3","Firebase","jenkins","Post man", "fork", "sourcetree", "robo 3T"
+      ],
+      icon: "fas fa-tools",
+    },
+    {
+      title: "design",
+      info: ["Microsoft Office" ,"Figma", "Photoshop", "Canva"],
+      icon: "fa fa-pencil-square-o",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Web IoT",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/todo-application/one_2.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/todo-application/two.png"),
+        },
+      ],
+      technologies: ["Vue 2", "firebase"],
+      category: "Web Monitor sensor Soil ",
+      github: "https://github.com/hozer006/new-pzem004t-code3",
+      visit: "https://phase-72507.web.app/",
+      description:
+        "Todo Application was made to help me with the learning schedule and it seams was doing a great job this app helping me to organize all the daily tasks.",
+    },
+
+    {
+      name: "Web IoT",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/borrow/Screenshot 2023-04-16 112241.png"),
+        },
+        {
+          // img: require("./src/assets/portfolio/todo-application/two.png"),
+        },
+      ],
+      technologies: ["Angular", "Node"],
+      category: "Borrow Application",
+      github: "https://github.com/hozer006/new-pzem004t-code3",
+      visit: "http://159.89.196.54:6022/pages/transaction/borrow-items",
+      description:
+        "Application was made to help me with the learning schedule and it seams was doing a great job this app helping me to organize all the daily tasks.",
+    },
+
+    {
+      // name: "Grocery-listing-react",
+      // pictures: [
+      //   // {
+      //   //   img: require("./src/assets/portfolio/clumpr/logo.png")
+      //   // },
+      //   {
+      //     img: require("./src/assets/portfolio/grocery/one.png"),
+      //   },
+      //   {
+      //     img: require("./src/assets/portfolio/grocery/two.png"),
+      //   },
+      // ],
+      // technologies: ["CSS", "JavaScript", "React.js", "HTML5"],
+      // category: "Web App",
+      // github: "https://github.com/Natain/grocery-listing-react",
+      // date: "",
+      // visit: "https://grocery-listing-react.netlify.app/",
+      // description:
+      //   "Grocery-listing-react it's a simple app that can help you doing shoppings.",
+    },
+    {
+    //   name: "Gitscores",
+    //   pictures: [
+    //     {
+    //       img: require("./src/assets/portfolio/Gitscores/one.png"),
+    //     },
+    //     {
+    //       img: require("./src/assets/portfolio/Gitscores/two.png"),
+    //     },
+    //     {
+    //       img: require("./src/assets/portfolio/Gitscores/three.png"),
+    //     },
+    //   ],
+    //   technologies: ["JavaScript", "CSS", "EJS", "Herokuapp"],
+    //   category: "Scoring app for Github",
+    //   github: "https://github.com/Natain/gitscores",
+    //   date: "Aug, 2021 - Sep, 2021",
+    //   visit: "https://gitscores.herokuapp.com/",
+    //   description:
+    //     "Gitscores v1.0.2 was updated from the orginal with more tools that let you personalize the app on your desingn.",
+    },
+  ],
+  recommendations: [
+    {
+      title:
+        "In his internship, Anuwat Rattanawong has demonstrated excellent learning ability, and with a dedicated, task oriented approach, he was able to complete his assignment ahead of time.",
+      author: "Asst. Prof. Dr. Rawee Promluangsri",
+      position: "Lecturer, Computer Communication Engineering",
+      company: "Udon Thani Rajabhat University",
+      location: "Udon Thani",
+    },
+    {
+      title:
+        "I feel his budding leadership abilities will become even more effective in a diverse and challenging environment.",
+      author: "Teacher Thanaphat Chiewwattana",
+      position: "Head of Computer Communication Engineering",
+      company: "Udon Thani Rajabhat Universitys",
+      location: "Udon Thani",
+    },
+  ],
+};
+
+export default info;
